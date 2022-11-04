edec6e669679e139b17a684541350de7fe8102e5
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-04 23:52:21 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "28563a75-6421-45cc-be5c-1e9e9b74a2da",
+    "home_feed": [
+        {
+            "id": "d21335d3-f30d-4db6-99ff-42584fab1c93",
+            "artist_name": "every",
+            "home_carousel": [
+                {
+                    "id": "8e241021-065b-45f5-a6e1-f443b7a1859a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                },
+                {
+                    "id": "2211d3fd-cd59-4813-be83-e9e3f2454ee1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "fe0eba58-1777-4627-8c9e-70d6f319841e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                },
+                {
+                    "id": "e6030e52-d89a-406f-ac27-1c6d2f03a63c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                },
+                {
+                    "id": "002c6f16-f459-430a-9786-4c0892f10682",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+                }
+            ],
+            "art_caption": "capital",
+            "art_description": "Answer everybody item concern. Particularly center parent recent let speech base. Enjoy we soon radio well likely."
+        },
+        {
+            "id": "07f6a35f-6722-4658-8a20-0850b1ad16ba",
+            "artist_name": "station",
+            "home_carousel": [
+                {
+                    "id": "a45ff417-5b68-4b67-881d-92122f39e343",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "e63fc201-b077-4588-b3b9-0427a4cd540a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "77a6fcfa-5f03-4895-9a4a-54e9baffb793",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/147.png"
+                },
+                {
+                    "id": "1bcb6018-144e-435e-8bc0-38f5c2b09fe0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "22e12fdb-3759-4fc6-84e3-32c6250d3da9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                }
+            ],
+            "art_caption": "believe",
+            "art_description": "Also song letter once pull campaign. Onto military start during message local teach third. Type mention method cover ask. Officer dog life hotel."
+        },
+        {
+            "id": "80678353-5288-4c9b-8028-61330dda239c",
+            "artist_name": "act",
+            "home_carousel": [
+                {
+                    "id": "ad231938-bc3a-4486-8039-481c899e62c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png"
+                },
+                {
+                    "id": "b3dbdc92-71b9-49ab-ace3-c47f2b8a6f96",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                },
+                {
+                    "id": "cb91d873-7a57-4814-ae08-14946e411ba4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                },
+                {
+                    "id": "8144a090-fb55-4064-ab8d-2ee39e2c9b21",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/157.png"
+                },
+                {
+                    "id": "4fde2828-de60-4176-844f-0b47f292d9a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                }
+            ],
+            "art_caption": "already",
+            "art_description": "Actually position class PM worry company kind change. Put old radio television treat. Audience raise any even leader church director."
+        },
+        {
+            "id": "2275cab4-156c-473f-974b-1745fbd149d2",
+            "artist_name": "as",
+            "home_carousel": [
+                {
+                    "id": "41f7c340-7f09-4578-9c32-cf8618a44e03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "6e8db738-3e89-4bc1-a55c-291b25746f1e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "679e9025-903a-4271-92df-6991d51fd32d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "cc6263a3-a6c8-4ea6-8841-6a694e289a43",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "3a76d162-de0b-4c87-98f9-9bcc4483a188",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                }
+            ],
+            "art_caption": "seat",
+            "art_description": "Final help trial letter. Box station ask."
+        },
+        {
+            "id": "d7e01ce9-3393-4e77-bc0f-0284ee657266",
+            "artist_name": "pretty",
+            "home_carousel": [
+                {
+                    "id": "b5ffbe43-ff96-4287-a051-468a8756f109",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/189.png"
+                },
+                {
+                    "id": "b6467e11-505b-4a58-844d-efb3a968a768",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "b6975ba2-cef9-4e6a-b2cb-10f369a3d7ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "c6c7cd81-1107-4234-98c5-316cfec1d7ab",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+                },
+                {
+                    "id": "4176cf7b-931b-476b-9d46-c2bcc995c709",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                }
+            ],
+            "art_caption": "seven",
+            "art_description": "Second anyone election have beyond situation. Remember feeling produce television. Pick team them necessary."
+        }
+    ]
+}
